43b002735f07d2beadb89f86158219fc3b070d21
Abbas463 <abbashilal463@gmail.com>
2026-02-24 11:51:15 +0430

    Initial commit

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import *
 
+class MyWebBrowser(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MyWebBrowser, self).__init__(*args, **kwargs)
+
+        self.window = QWidget()
+        self.window.setWindowTitle("AFG Browser")
+
+        self.layout = QVBoxLayout()
+        self.horizontal = QHBoxLayout()
